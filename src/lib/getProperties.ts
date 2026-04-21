a1d79a17f8415932051b6c8899396c1c4c878178
Shahida <direction@upskillz.fr>
2026-04-21 17:27:47 +0200

    geocoding map

--- src/lib/getProperties.ts
+++ src/lib/getProperties.ts
@@ -2,18 +2,18 @@ import { parseHektorCSV } from "./hektorParser";
 
 // 🔥 LISTE DES BIENS
 export async function getProperties() {
-  return parseHektorCSV();
+  return await parseHektorCSV();
 }
 
 // 🔥 DETAIL D’UN BIEN
 export async function getPropertyById(id: string) {
-  const properties = parseHektorCSV();
+  const properties = await parseHektorCSV(); // ✅ FIX
 
   if (!id) return undefined;
 
-return properties.find(
-  (p) =>
-    p?.id &&
-    p.id.toString().trim() === id.toString().trim()
-);
+  return properties.find(
+    (p) =>
+      p?.id &&
+      p.id.toString().trim() === id.toString().trim()
+  );
 }
